316eb3f18fed3df5908cda95ba66d8b3b2a9d68d
not-abdulbari <146872928+not-abdulbari@users.noreply.github.com>
2025-03-25 23:22:12 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 session_start();
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+ini_set('log_errors', 1);
+ini_set('error_log', __DIR__ . '/error.log');
+error_log('Error log test');
 
 $show_alert = false; // Flag to control alert display
 
