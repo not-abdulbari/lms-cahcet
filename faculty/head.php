7aab1fc4b0e61b8a80aeeb4b2b2ce91165f6f862
not-abdulbari <146872928+not-abdulbari@users.noreply.github.com>
2025-04-09 10:27:17 +0530

    Update head.php

--- faculty/head.php
+++ faculty/head.php
@@ -213,6 +213,7 @@ if (!function_exists('getCurrentDateTime')) {
                 <a href="javascript:void(0)">STUDENT</a>
                 <div class="dropdown-content">
                     <a href="parent.php">STUDENT LOGIN</a>
+                    <a href="../student/student_login.php">STUDENT LOGIN</a>
                     <a href="student_profile.php">Under Development</a>
                 </div>
             </li>
