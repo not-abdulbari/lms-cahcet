ceb15141c1032282cd5c76718b1662927f7248aa
ABDUL BARI N <146872928+not-abdulbari@users.noreply.github.com>
2025-04-26 00:25:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -241,7 +241,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['roll_no'])) {
         }
         input[type="password"]::-ms-reveal,
         input[type="password"]::-ms-clear {
-            width: 90%;
+            width: 100%;
             padding: 10px;
             margin: 10px 0;
             border: 2px solid #ddd;
