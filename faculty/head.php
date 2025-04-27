08a1ea0e3d6bd068a418635b2ee99665f5e7ce00
ABDUL BARI N <146872928+not-abdulbari@users.noreply.github.com>
2025-04-27 18:20:17 +0530

    Update head.php

--- faculty/head.php
+++ faculty/head.php
@@ -206,6 +206,7 @@ if (!function_exists('getCurrentDateTime')) {
                     <a href="generate_namelist.php">Students Namelist</a>
                     <a href="consolidated_marklist.php">Consolidated Marklist</a>
                     <a href="university_results.php">University Progress Report</a>
+                    <a href="parent_feedback.php">PARENT FEEDBACK</a>
                 </div>
             </li>
             <li><a href="student_login.php">STUDENT LOGIN</a></li>
