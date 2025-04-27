9298ed6d8f158e7ba7e0546b34d8e06a5904b69a
ABDUL BARI N <146872928+not-abdulbari@users.noreply.github.com>
2025-04-27 18:27:52 +0530

    Update feedback_namelist.php

--- faculty/feedback_namelist.php
+++ faculty/feedback_namelist.php
@@ -104,7 +104,7 @@ th {
                     <td><?= htmlspecialchars($student['roll_no']) ?></td>
                     <td><?= htmlspecialchars($student['name']) ?></td>
                     <td>
-                        <button type="submit" class="btn">href="feedback_generate.php?roll_no=<?= $student['roll_no'] ?>" target="_blank">Download Feedback</button>
+                    <button type="button" class="btn" onclick="window.open('feedback_generate.php?roll_no=<?= $student['roll_no'] ?>', '_blank')">Download Feedback</button>
                     </td>
                 </tr>
             <?php endwhile; ?>
