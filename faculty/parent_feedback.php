701c7d482dec8328f90aaf6958b691ec881213bb
ABDUL BARI N <146872928+not-abdulbari@users.noreply.github.com>
2025-04-28 19:05:52 +0530

    Update parent_feedback.php

--- faculty/parent_feedback.php
+++ faculty/parent_feedback.php
@@ -259,6 +259,12 @@ body {
                     <?php endforeach; ?>
                 </select>
             </div>
+                        <!-- Add a checkbox for NBA logo -->
+            <div class="form-group">
+                <label>
+                    <input type="checkbox" name="nba_logo" value="1"> Is NBA logo needed?
+                </label>
+            </div>
             <div class="form-group">
                 <button type="submit">Submit</button>
             </div>
