f7ad0960ab6e9fd4a79bc43e19ff9f469595d71e
ABDUL BARI N <146872928+not-abdulbari@users.noreply.github.com>
2025-04-28 19:35:20 +0530

    Create footer.php

---
+++ faculty/footer.php
@@ -0,0 +1,3 @@
+<footer style="text-align: center; padding: 15px; background: #1f2937; color: #ffffff; font-size: 14px; width: 100%; bottom: 0; position: fixed; box-shadow: 0 -2px 10px rgba(0, 0, 0, 0.1);">
+    <p style="margin: 5px 0; font-weight: bold;">By the CSE Class of '26</p>
+</footer>
